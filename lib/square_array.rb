614ed99e014dbaacf1419b77bba64e31aa23c948
Jason Viola <jason.viola@mavs.uta.edu>
2019-06-29 18:29:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
-  new_numbers = []
+  new_numbers = [ ]
   counter = 0
   while array[counter] < 2 do 
     new_numbers.push(array[counter]) 
+    counter += 1
   end
   return new_numbers
 end  
